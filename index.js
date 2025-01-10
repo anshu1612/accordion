aa2ec7e704f1108c54a656d70cdb3798356b9352
anshu1612 <anshuchakrawarti26@gmail.com>
2025-01-10 23:13:48 +0530

    automoatically hide others

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const icons=document.querySelectorAll("img");
-const contents=document.querySelectorAll("p")
-const content3=document.getElementById("content3")
-icons.forEach((icon,index)=>{
-    icon.addEventListener("click",()=>{
-        let content=contents[index];
-        content.classList.toggle("hidden");
-        icon.classList.toggle("rotate-90")
-        document.getElementById("div3").classList.remove("rounded-b-lg")
-        content3.classList.add("rounded-b-lg");
-    })
-})
+const icons = document.querySelectorAll("img");
+const contents = document.querySelectorAll("p");
+const content3 = document.getElementById("content3");
+let i = 0;
+icons.forEach((icon, index) => {
+  icon.addEventListener("click", () => {
+    contents.forEach((text, i) => {
+      if (i != index) {
+        text.classList.add("hidden");
+        icons[i].classList.remove("rotate-90");
+      }
+    });
+
+    let content = contents[index];
+    content.classList.toggle("hidden");
+    icon.classList.toggle("rotate-90");
+    document.getElementById("div3").classList.remove("rounded-b-lg");
+    content3.classList.add("rounded-b-lg");
+  });
+});
